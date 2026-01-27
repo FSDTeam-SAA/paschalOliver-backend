d896188439a4bca0f20e4fa79d58f041d3163456
SaniMohibur <mohiburrahmansani@gmail.com>
2026-01-27 22:30:15 +0600

    add auth in route

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { CategoryControllers } from './category.controller';
+import auth from '../../middlewares/auth';
+import { userRole } from '../user/user.constant';
 
 const router = express.Router();
 
 // Route: /api/v1/categories
-router.post('/create-category', CategoryControllers.createCategory);
+router.post(
+  '/create-category',
+  auth(userRole.admin),
+  CategoryControllers.createCategory,
+);
 router.get('/', CategoryControllers.getAllCategories);
-router.delete('/:id', CategoryControllers.deleteCategory);
+router.delete('/:id', auth(userRole.admin), CategoryControllers.deleteCategory);
 
 export const CategoryRoutes = router;
--- src/app/modules/subcategory/subcategory.route.ts
+++ src/app/modules/subcategory/subcategory.route.ts
@@ -1,15 +1,25 @@
 import express from 'express';
 import { SubcategoryControllers } from './subcategory.controller';
+import auth from '../../middlewares/auth';
+import { userRole } from '../user/user.constant';
 
 const router = express.Router();
 
 // Route: /api/v1/subcategories
-router.post('/create-subcategory', SubcategoryControllers.createSubcategory);
+router.post(
+  '/create-subcategory',
+  auth(userRole.admin),
+  SubcategoryControllers.createSubcategory,
+);
 router.get('/', SubcategoryControllers.getAllSubcategories);
 router.get(
   '/category/:categoryId',
   SubcategoryControllers.getSubcategoriesByCategoryId,
 );
-router.delete('/:id', SubcategoryControllers.deleteSubcategory);
+router.delete(
+  '/:id',
+  auth(userRole.admin),
+  SubcategoryControllers.deleteSubcategory,
+);
 
 export const SubcategoryRoutes = router;
