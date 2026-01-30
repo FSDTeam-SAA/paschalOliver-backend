4617edee5402b870609b838704bb5d755878e3c7
Md. Moniruzzaman <monir.cse6.bu@gmail.com>
2026-01-30 22:05:48 +0600

    for ci/cd

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use('/api/v1', router);
 
 // Root router
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Welcome to the server' });
+  res.status(200).json({ message: 'Welcome to the from monir' });
 });
 
 // Not found route
